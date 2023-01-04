253f378e2b961c8f63fbfd708ca536ccad963b1c
Barnabas Bulpett <barnabasbulpett@gmail.com>
2023-01-03 19:53:52 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+while True:
+    ask = input('Question: ')
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=ask,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    text = response['choices'][0]['text']
+
+    print('Reply: ' + text)
